aef699039b13d863a83ce394f76df3646864bc9a
Vinicio Valbuena <formatcomvinicio@gmail.com>
2019-09-10 05:29:11 -0500

    add name image in docker hub

--- run.sh
+++ run.sh
@@ -22,21 +22,35 @@
 : ${DOWN_AFTER:=5000}
 : ${FAILOVER_TIMEOUT:=60000}
 : ${PARALLEL_SYNCS:=1}
+: ${REDIS_PORT:=6379}
+: ${MASTER_PORT:=6379}
+: ${SENTINEL_HOST:=redis-sentinel}
+: ${SENTINEL_PORT:=26379}
 
 function launchmaster() {
-  exec redis-server /usr/local/etc/redis/redis.conf
+  exec redis-server /usr/local/etc/redis/redis.conf --port ${REDIS_PORT}
 }
 
 function launchsentinel() {
   while true; do
-    master=$(redis-cli -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name ${MASTER_NAME} | tr ',' ' ' | cut -d' ' -f1)
+
+    echo "Find master..."
+    master=$(timeout 10 redis-cli -h ${SENTINEL_HOST} -p ${SENTINEL_PORT} --csv SENTINEL get-master-addr-by-name ${MASTER_NAME} | tr ',' ' ' | cut -d' ' -f1)
     if [[ -n ${master} ]]; then
+      echo "Mode SENTINEL"
       master="${master//\"}"
     else
       master=$(hostname -i)
     fi
 
-    redis-cli -h ${master} INFO
+    if [[ ! -z "${MASTER_HOST}" ]]; then
+      echo "Mode MASTER_HOST"
+      master=${MASTER_HOST}
+    else
+      echo "Mode HOSTNAME"
+    fi
+
+    redis-cli -h ${master} -p ${MASTER_PORT} INFO
     if [[ "$?" == "0" ]]; then
       break
     fi
@@ -44,45 +58,53 @@ function launchsentinel() {
     sleep 10
   done
 
-  sentinel_conf=sentinel.conf
+  sentinel_conf=/tmp/sentinel.conf
 
-  echo "sentinel monitor ${MASTER_NAME} ${master} 6379 ${QUORUM}" > ${sentinel_conf}
+  echo "sentinel monitor ${MASTER_NAME} ${master} ${MASTER_PORT} ${QUORUM}" > ${sentinel_conf}
   echo "sentinel down-after-milliseconds ${MASTER_NAME} ${DOWN_AFTER}" >> ${sentinel_conf}
   echo "sentinel failover-timeout ${MASTER_NAME} ${FAILOVER_TIMEOUT}" >> ${sentinel_conf}
   echo "sentinel parallel-syncs ${MASTER_NAME} ${PARALLEL_SYNCS}" >> ${sentinel_conf}
   echo "bind 0.0.0.0" >> ${sentinel_conf}
 
-  exec redis-sentinel ${sentinel_conf} --protected-mode no
+  exec redis-sentinel ${sentinel_conf} --port ${SENTINEL_PORT} --protected-mode no
 }
 
 function launchslave() {
   while true; do
-    master=$(redis-cli -h ${REDIS_SENTINEL_SERVICE_HOST} -p ${REDIS_SENTINEL_SERVICE_PORT} --csv SENTINEL get-master-addr-by-name ${MASTER_NAME} | tr ',' ' ' | cut -d' ' -f1)
-    if [[ -n ${master} ]]; then
-      master="${master//\"}"
+    echo "Find master..."
+    if [[ ! -z "${MASTER_HOST}" ]]; then
+      echo "Mode MASTER_HOST"
+      master=${MASTER_HOST}
     else
-      echo "Failed to find master."
-      sleep 60
-      exit 1
+      echo "Mode SENTINEL"
+      master=$(timeout 10 redis-cli -h ${SENTINEL_HOST} -p ${SENTINEL_PORT} --csv SENTINEL get-master-addr-by-name ${MASTER_NAME} | tr ',' ' ' | cut -d' ' -f1)
+      if [[ -n ${master} ]]; then
+        master="${master//\"}"
+      else
+	echo "Could not connect to Redis Sentinel at ${SENTINEL_HOST}:${SENTINEL_PORT}: Connection refused"
+        sleep 10
+	continue
+      fi
     fi
-    redis-cli -h ${master} INFO
+
+    redis-cli -h ${master} -p ${MASTER_PORT} INFO
     if [[ "$?" == "0" ]]; then
       break
     fi
     echo "Connecting to master failed.  Waiting..."
     sleep 10
   done
-  exec redis-server /usr/local/etc/redis/redis.conf --slaveof ${master} 6379
+  exec redis-server /usr/local/etc/redis/redis.conf --port ${REDIS_PORT} --slaveof ${master} ${MASTER_PORT}
 }
 
 sed -i "s/%tcp-backlog%/${TCP_BACKLOG}/" /usr/local/etc/redis/redis.conf
 
-if [[ "${MASTER}" == "true" ]]; then
-  launchmaster
+if [[ "${SLAVE}" == "true" ]]; then
+  launchslave
 fi
 
 if [[ "${SENTINEL}" == "true" ]]; then
   launchsentinel
 fi
 
-launchslave
+launchmaster
